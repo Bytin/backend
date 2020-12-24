5d275a495bc4e020cd2972df5656f9d848c12f30
Ghislain Deffo <g.avet.gnaritas@gmail.com>
2020-12-24 17:56:44 -0500

    added a debug endpoint to show a user's authorites

--- src/main/java/tech/bytin/api/ApiApplication.java
+++ src/main/java/tech/bytin/api/ApiApplication.java
@@ -2,12 +2,21 @@ package tech.bytin.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
+        }
+        
+        @RequestMapping("/hello")
+        public String hello(){
+               return "Hi there, " + SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        }
 
 }
--- src/main/java/tech/bytin/api/controller/UserController.java
+++ src/main/java/tech/bytin/api/controller/UserController.java
@@ -1,12 +1,12 @@
 package tech.bytin.api.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 import core.boundary.UserIOBoundary;
 import core.usecase.user.CreateUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
         private final UserIOBoundary userInteractor;
-
-        @Autowired
-        PasswordEncoder passwordEncoder;
-
-        UserController(@Autowired UserIOBoundary userIOBoundary) {
-                this.userInteractor = userIOBoundary;
-        }
+        private final PasswordEncoder passwordEncoder;
 
         @PostMapping(value = "/register")
         public ResponseEntity<?> createUser(@RequestBody CreateUser.RequestModel requestModel)
