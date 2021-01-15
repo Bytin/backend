0daa305d44ff28ffd910b444a5852b46150e584f
Ghislain Deffo <g.avet.gnaritas@gmail.com>
2021-01-15 13:16:14 -0500

    handled exception when updating admin data in database

--- src/main/java/tech/bytin/api/util/hardCoded/Admin.java
+++ src/main/java/tech/bytin/api/util/hardCoded/Admin.java
@@ -2,6 +2,7 @@ package tech.bytin.api.util.hardCoded;
 
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import core.entity.User.UserRole;
@@ -19,9 +20,8 @@ public class Admin {
 
     @Autowired
     JpaUserRepository userRepo;
-    
-    @PostConstruct
-    void put(){
+
+    private void put() {
         var adminData = props.getAdmin();
         UserJpaEnity admin = new UserJpaEnity();
         admin.setUsername(adminData.get("username"));
@@ -29,5 +29,12 @@ public class Admin {
         admin.setPassword(encoder.encode(adminData.get("password")));
         admin.setRole(UserRole.ADMIN);
         userRepo.save(admin);
+    }
+
+    @PostConstruct
+    void tryExceptWhenAdminAlreayExists() {
+        try {
+            put();
+        } catch (DataIntegrityViolationException e) { }
     }
 }
