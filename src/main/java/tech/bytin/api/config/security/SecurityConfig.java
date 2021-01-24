7d7a8d9c093856f81628705a882dec3b6c4ec3dd
Ghislain Deffo <g.avet.gnaritas@gmail.com>
2021-01-24 00:05:16 -0500

    added cors configuration

--- src/main/java/tech/bytin/api/config/security/SecurityConfig.java
+++ src/main/java/tech/bytin/api/config/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package tech.bytin.api.config.security;
 
+import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import core.gateway.UserGateway;
 import tech.bytin.api.util.EntityMapper;
 
@@ -30,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+
         http.authorizeRequests().antMatchers("/login", "/user/register", "/user/activate", "/hello",
                 "/snippets/public/**").permitAll().anyRequest().authenticated();
 
@@ -51,6 +58,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(username -> new SecurityUser(
                 repo.findByUserName(username).map(EntityMapper::mapUserToJpaEntity).orElseThrow(
                         () -> new UsernameNotFoundException("Unable to find that user"))));
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(@Value("${web.location}") String origin) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(origin));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
+        configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
